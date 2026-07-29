ccf70269cb89e947bf4a7a4d5899cf826edf6086
Bhoomika M <bhoomikambhuvi293@gmail.com>
2026-07-29 09:49:37 +0530

    First Commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/oops/MethodOverloadingIn.java
@@ -0,0 +1,39 @@
+package org.tnsif.acc.c2tc.oops;
+
+class Payment{
+	void makePayment() {
+		System.out.println("Processing payment using generic method");
+	}
+}
+class UpiPayment extends Payment{
+	void makePayment() {
+		System.out.println("Payment is done using UPI ");
+	}
+}
+class CardPayment extends Payment{
+	void makePayment() {
+		System.out.println("payment is made using credit/debit card");
+	}
+}
+class WalletPayment extends Payment{
+	void makePayment() {
+		System.out.println("payment is done via wallet");
+	}
+}
+public class MethodOverloadingIn {
+
+	public static void main(String[] args) {
+		Payment payment;//reference parent class
+		
+		payment=new UpiPayment();
+		payment.makePayment();
+		
+		payment=new CardPayment();
+		payment.makePayment();
+		
+		payment=new WalletPayment();
+		payment.makePayment();	
+
+	}
+
+}
