fb9c5c0f98582dbc7dc29c7c24f91814b5122551
Bhoomika M <bhoomikambhuvi293@gmail.com>
2026-07-30 10:38:46 +0530

    First Commit

---
+++ TNSIF_Training/src/org/tnsif/acc/c2tc/methodoverriding_super_this_instanceof/InstanceofDemo2.java
@@ -0,0 +1,39 @@
+package org.tnsif.acc.c2tc.methodoverriding_super_this_instanceof;
+
+class PaymentMethod {
+	void makePayment() {
+		System.out.println("Processing Payment");
+	}
+}
+class CreditCard extends PaymentMethod{
+	void swipeCard() {
+		System.out.println("Swiping Card");
+	}
+}
+class PayPal extends PaymentMethod{
+	void loginToPayPal() {
+		System.out.println("Login to paypal ");
+	}
+}
+
+
+
+public class InstanceofDemo2 {
+
+	public static void main(String[] args) {
+		
+		PaymentMethod payment=new PaymentMethod();
+		payment =new CreditCard();
+		if (payment instanceof CreditCard) {
+			CreditCard cc=(CreditCard)payment;
+			cc.swipeCard();
+		}
+		else if(payment instanceof PayPal)
+		{
+			PayPal pp =(PayPal)payment;
+			pp.loginToPayPal();
+		}
+
+	}
+
+}
